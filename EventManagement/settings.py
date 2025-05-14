0c77512936dce2cd937627458040044bc08a379d
khalid <109499271+Khalidmuhammad25@users.noreply.github.com>
2025-05-14 13:32:46 +0600

    db

--- EventManagement/settings.py
+++ EventManagement/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,15 +86,24 @@ WSGI_APPLICATION = 'EventManagement.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'EventManagement',
+#         'USER': 'postgres',
+#         'PASSWORD':'almighty',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'EventManagement',
-        'USER': 'postgres',
-        'PASSWORD':'almighty',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://task_management_4pbj_user:6EleRfi3KcvD3MK9wgmJi1Q5XxFc7dCy@dpg-d0i43midbo4c73dh2m50-a.oregon-postgres.render.com/task_management_4pbj',
+        conn_max_age=600
+    )
 }
 
 # Password validation
